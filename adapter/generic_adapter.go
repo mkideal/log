94fdfc00d8ef750ed3650cac2935ce65049594b0
mkideal <i@mkideal.com>
2016-09-18 14:08:53 +0800

    add logger adapter for labstack logger

--- adapter/generic_adapter.go
+++ adapter/generic_adapter.go
@@ -1,10 +1,10 @@
-package adaptor
+package adapter
 
 import (
 	"github.com/mkideal/log"
 )
 
-// AdaptorLogger implements generic logger:
+// GenericLogger ixmplements generic logger:
 //
 //	type Logger interface {
 //		Trace(...interface{})
@@ -25,52 +25,52 @@ import (
 //		Fatal(...interface{})
 //		Fatalf(string,...interface{})
 //	}
-type AdaptorLogger byte
+type GenericLogger byte
 
-func (l AdaptorLogger) Trace(msg ...interface{}) {
+func (l GenericLogger) Trace(msg ...interface{}) {
 	log.Print(3, log.LvTRACE, msg...)
 }
 
-func (l AdaptorLogger) Tracef(format string, args ...interface{}) {
+func (l GenericLogger) Tracef(format string, args ...interface{}) {
 	log.Printf(3, log.LvTRACE, format, args...)
 }
 
-func (l AdaptorLogger) Debug(msg ...interface{}) {
+func (l GenericLogger) Debug(msg ...interface{}) {
 	log.Print(3, log.LvDEBUG, msg...)
 }
 
-func (l AdaptorLogger) Debugf(format string, args ...interface{}) {
+func (l GenericLogger) Debugf(format string, args ...interface{}) {
 	log.Printf(3, log.LvDEBUG, format, args...)
 }
 
-func (l AdaptorLogger) Info(msg ...interface{}) {
+func (l GenericLogger) Info(msg ...interface{}) {
 	log.Print(3, log.LvINFO, msg...)
 }
 
-func (l AdaptorLogger) Infof(format string, args ...interface{}) {
+func (l GenericLogger) Infof(format string, args ...interface{}) {
 	log.Printf(3, log.LvINFO, format, args...)
 }
 
-func (l AdaptorLogger) Warn(msg ...interface{}) {
+func (l GenericLogger) Warn(msg ...interface{}) {
 	log.Print(3, log.LvWARN, msg...)
 }
 
-func (l AdaptorLogger) Warnf(format string, args ...interface{}) {
+func (l GenericLogger) Warnf(format string, args ...interface{}) {
 	log.Printf(3, log.LvWARN, format, args...)
 }
 
-func (l AdaptorLogger) Error(msg ...interface{}) {
+func (l GenericLogger) Error(msg ...interface{}) {
 	log.Print(3, log.LvERROR, msg...)
 }
 
-func (l AdaptorLogger) Errorf(format string, args ...interface{}) {
+func (l GenericLogger) Errorf(format string, args ...interface{}) {
 	log.Printf(3, log.LvERROR, format, args...)
 }
 
-func (l AdaptorLogger) Fatal(msg ...interface{}) {
+func (l GenericLogger) Fatal(msg ...interface{}) {
 	log.Print(3, log.LvFATAL, msg...)
 }
 
-func (l AdaptorLogger) Fatalf(format string, args ...interface{}) {
+func (l GenericLogger) Fatalf(format string, args ...interface{}) {
 	log.Printf(3, log.LvFATAL, format, args...)
 }
