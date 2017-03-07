6e9caca23a8f74a717d43d27168bba1bd2eb2b5a
mkideal <i@mkideal.com>
2017-03-07 16:19:40 +0800

    add method Set(string) for logger.Level

--- logger/log_level.go
+++ logger/log_level.go
@@ -10,17 +10,41 @@ import (
 type Level int32
 
 const (
-	FATAL Level = iota
-	ERROR
-	WARN
-	INFO
-	DEBUG
-	TRACE
-	NumLevel
+	FATAL    Level = iota // 0
+	ERROR                 // 1
+	WARN                  // 2
+	INFO                  // 3
+	DEBUG                 // 4
+	TRACE                 // 5
+	NumLevel              // 6 log levels
 )
 
 var ErrUnrecognizedLogLevel = errors.New("unrecognized log level")
 
+// Set implements flag.Value interface such that you can use level  as a command as following:
+//
+//	var level logger.Level
+//	flag.Var(&level, "log_level", "log level: trace/debug/info/warn/error/fatal")
+func (level *Level) Set(s string) error {
+	return level.Decode(s)
+}
+
+// Decode implements github.com/mkideal/cli.Decoder interface
+func (level *Level) Decode(s string) error {
+	lv, ok := ParseLevel(s)
+	*level = lv
+	if !ok {
+		return ErrUnrecognizedLogLevel
+	}
+	return nil
+}
+
+// Literal returns literal value which is a number
+func (level Level) Literal() string {
+	return strconv.Itoa(int(level))
+}
+
+// String returns a serialized string of level
 func (level Level) String() string {
 	switch level {
 	case FATAL:
@@ -63,40 +87,31 @@ func (level *Level) UnmarshalJSON(data []byte) error {
 
 func (level Level) MoreVerboseThan(other Level) bool { return level > other }
 
-func (level *Level) Decode(s string) error {
-	lv, ok := ParseLevel(s)
-	*level = lv
-	if !ok {
-		return ErrUnrecognizedLogLevel
-	}
-	return nil
-}
-
 // ParseLevel parses log level from string
 func ParseLevel(s string) (lv Level, ok bool) {
 	s = strings.ToUpper(s)
 	switch s {
-	case "FATAL", "F", "0":
+	case "FATAL", "F", FATAL.Literal():
 		return FATAL, true
-	case "ERROR", "E", "1":
+	case "ERROR", "E", ERROR.Literal():
 		return ERROR, true
-	case "WARN", "W", "2":
+	case "WARN", "W", WARN.Literal():
 		return WARN, true
-	case "INFO", "I", "3":
+	case "INFO", "I", INFO.Literal():
 		return INFO, true
-	case "DEBUG", "D", "4":
+	case "DEBUG", "D", DEBUG.Literal():
 		return DEBUG, true
-	case "TRACE", "T", "5":
+	case "TRACE", "T", TRACE.Literal():
 		return TRACE, true
 	}
 	return INFO, false
 }
 
-// MustParseLevel similars to ParseLevel, but panic if parse fail
+// MustParseLevel similars to ParseLevel, but panic if parse failed
 func MustParseLevel(s string) Level {
 	lv, ok := ParseLevel(s)
 	if !ok {
-		panic("ParseLevel " + s + " fail")
+		panic(ErrUnrecognizedLogLevel.Error() + ": " + s)
 	}
 	return lv
 }
